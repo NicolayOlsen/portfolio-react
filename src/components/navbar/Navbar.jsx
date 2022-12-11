5bc3092c523ee866758343dd6543c6a01aaa9f3c
NicolayOlsen <nicolay2003.nao@gmail.com>
2022-12-11 18:56:29 +0100

    redesigned website using react

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo-dark.png"
+import "./navbar.css";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#about">About</a>
+    </p>
+    <p>
+      <a href="#skills">Skills</a>
+    </p>
+    <p>
+      <a href="#projects">Projects</a>
+    </p>
+    <p>
+      <a href="#contact">Contact</a>
+    </p>
+  </>
+);
+// BEM --> Block Element Modifier
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className="portfolio__navbar">
+      <div className="portfolio__navbar-links">
+          <h1>Portfolio</h1>
+        <div className="portfolio__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+    
+      <div className="portfolio__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+        ) : (
+          <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <div className="portfolio__navbar-menu_container scale-up-center">
+            <div className="portfolio__navbar-menu_container-links"></div>
+            <Menu />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./header.css"
+import react from "../../assets/react.png"
+import tailwind from "../../assets/tailwind.png"
+
+const Header = () => {
+
+    document.addEventListener("mousemove", parallax);
+    function parallax(e){
+      document.querySelectorAll(".portfolio__header-content img").forEach(function(move){
+  
+        var moving_value = move.getAttribute("data-value");
+        var x = (e.clientX * moving_value) / 300;
+        var y = (e.clientY * moving_value) / 300;
+  
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px";
+      });
+    }
+
+  return (
+    <div className='portfolio__header section__padding' id="home">
+      <div className='portfolio__header-content'>
+        <img src={react} data-value="1" alt="react" />
+        <img src={tailwind} data-value="2" alt="tailwind" />
+        <h1>Hi👋,<br/>I'm <span className="gradient__text">Nicolay Andre Olsen</span><br/> Frontend Developer</h1>
+        <a href=''>Download CV</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
